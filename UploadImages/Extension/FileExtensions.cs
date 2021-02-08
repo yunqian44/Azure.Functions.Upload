9aa07f39796b91d79c0045809432e19c4070e98b
yunqian44 <yunqian8@live.com>
2021-02-08 17:48:02 +0800

    Add File Extension Method

---
+++ UploadImages/Extension/FileExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.StaticFiles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UploadImages.Extension
+{
+    public static class FileExtensions
+    {
+        private static readonly FileExtensionContentTypeProvider provider = new FileExtensionContentTypeProvider();
+
+        public static string GetContentType(this string fileName)
+        {
+            if (!provider.TryGetContentType(fileName, out var contentType))
+            {
+                contentType = "application/octet-stream";
+            }
+            return contentType;
+        }
+    }
+}
