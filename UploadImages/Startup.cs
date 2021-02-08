d1de38b7fbecc77adcfef8b4ab7d9c41652ff063
yunqian44 <yunqian8@live.com>
2021-02-08 17:49:59 +0800

    Add Azure Functions Dependency injection

---
+++ UploadImages/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UploadImages.Service;
+
+namespace UploadImages
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddTransient<BlobService, BlobService>();
+        }
+    }
+}
