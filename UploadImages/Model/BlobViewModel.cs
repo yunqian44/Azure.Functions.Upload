5e717d21508b6a4938dd06b4c871d772867fc8c3
yunqian44 <yunqian8@live.com>
2021-02-18 18:06:03 +0800

    add request ViewModel for Azure Functions of HttpTrigger

---
+++ UploadImages/Model/BlobViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UploadImages.Model
+{
+    public class BlobViewModel
+    {
+        /// <summary>
+        /// 文件路径
+        /// </summary>
+        public string FilePath { get; set; }
+
+        /// <summary>
+        /// 文件内容
+        /// </summary>
+        public string FileContent { get; set; }
+
+        /// <summary>
+        /// 文件名称
+        /// </summary>
+        public string FileName { get; set; }
+    }
+}
