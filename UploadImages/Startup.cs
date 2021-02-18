18e442e9c9a8b0fdf1253363d73b50e90fff43f2
yunqian44 <yunqian8@live.com>
2021-02-18 18:02:18 +0800

    Use dependency injection in .NET Azure Functions

--- UploadImages/Startup.cs
+++ UploadImages/Startup.cs
@@ -1,17 +1,30 @@
-﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+﻿using Azure.Storage.Blobs;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using UploadImages;
 using UploadImages.Service;
+
+[assembly: FunctionsStartup(typeof(Startup))]
 
 namespace UploadImages
 {
     public class Startup : FunctionsStartup
     {
+        //public Startup(IConfiguration configuration)
+        //{
+        //    Configuration = configuration;
+        //}
+
+        //public IConfiguration Configuration { get; }
+
         public override void Configure(IFunctionsHostBuilder builder)
         {
-            builder.Services.AddTransient<BlobService, BlobService>();
+            builder.Services.AddSingleton(x => new BlobServiceClient("storageaccount_connection"));
+            builder.Services.AddTransient<IBlobService, BlobService>();
         }
     }
 }
