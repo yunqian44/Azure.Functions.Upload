f944eb95f9517a4878a3999154768b2727a57904
yunqian44 <yunqian8@live.com>
2021-02-22 19:14:05 +0800

    Add an injection to the AppSettings Class

--- UploadImages/Startup.cs
+++ UploadImages/Startup.cs
@@ -1,11 +1,14 @@
 ﻿using Azure.Storage.Blobs;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using UploadImages;
+using UploadImages.Common;
 using UploadImages.Service;
 
 [assembly: FunctionsStartup(typeof(Startup))]
@@ -14,17 +17,12 @@ namespace UploadImages
 {
     public class Startup : FunctionsStartup
     {
-        //public Startup(IConfiguration configuration)
-        //{
-        //    Configuration = configuration;
-        //}
-
-        //public IConfiguration Configuration { get; }
-
         public override void Configure(IFunctionsHostBuilder builder)
         {
-            //builder.Services.AddSingleton(x => new BlobServiceClient("storageaccount_connection"));
-            builder.Services.AddSingleton(x => new BlobServiceClient("DefaultEndpointsProtocol=https;AccountName=cnbatestorageaccount;AccountKey=EsEUMqoZbqwrqXrtQcVGPL1p5FHFReIyObRAnVdZfCXXXXXXXXXXXXXXX3Eh4O/5NvfhDB0BF9C74osmnkagRWQ==;EndpointSuffix=core.windows.net"));
+            builder.Services.AddSingleton(new Appsettings(builder.GetContext().ApplicationRootPath));
+            builder.Services.AddSingleton(x => new BlobServiceClient(Appsettings.app("AzureBlobStorageConnectionString")));
+
+            //builder.Services.AddSingleton(x => new BlobServiceClient("DefaultEndpointsProtocol=https;AccountName=cnbatestorageaccount;AccountKey=EsEUMqoZbqwrqXrtQcVGPL1p5FHFReIyObRAnVdZfCXXXXXXXXXXXXXXX3Eh4O/5NvfhDB0BF9C74osmnkagRWQ==;EndpointSuffix=core.windows.net"));
             builder.Services.AddTransient<IBlobService, BlobService>();
         }
     }
